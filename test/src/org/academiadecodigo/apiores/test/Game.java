59d718fcce00339b7fa72bd4e18601a746b9b9ec
Miguel Farto <miguel_farto@hotmail.com>
2020-02-13 21:39:03 -0100

    added sound to the game class

--- test/src/org/academiadecodigo/apiores/test/Game.java
+++ test/src/org/academiadecodigo/apiores/test/Game.java
@@ -3,6 +3,7 @@ package org.academiadecodigo.apiores.test;
 import org.academiadecodigo.apiores.test.levels.*;
 import org.academiadecodigo.apiores.test.levels.LevelStructure;
 import org.academiadecodigo.apiores.test.mainmenu.MainMenu;
+import org.academiadecodigo.apiores.test.sound.Sound;
 import org.academiadecodigo.simplegraphics.pictures.Picture;
 
 public class Game {
@@ -12,9 +13,13 @@ public class Game {
 
     public void game() throws InterruptedException {
         MainMenu startMenu = new MainMenu();
+        Sound alleyCat = new Sound(" resources/05 Alleycat Blues (1).wav");
+        Sound shadow = new Sound(" resources/24-Shadow-Master.wav");
+        Sound corneria = new Sound(" resources/06 Corneria (1).wav");
+        Sound moon = new Sound(" resources/09-the-moon.wav");
 
         while (true) {
-
+            alleyCat.play(true);
             startMenu.startMenu();
 
             currentLevel = 0;
@@ -27,12 +32,17 @@ public class Game {
             if (LevelStructure.isGameOver()) {
                 continue;
             }
+
+            alleyCat.stop();
+            shadow.play(true);
             currentLevel++;
             levels[currentLevel].start();
             if(LevelStructure.isGameOver()){
                 continue;
             }
 
+            shadow.stop();
+            corneria.play(true);
             currentLevel++;
             levels[currentLevel].start();
             if(LevelStructure.isGameOver()){
